ebb9ef31a4556741ab99c414c0a09e724d1642ac
inCrafts <inCrafts@mail.ru>
2019-10-17 21:21:16 +0300

    Пересчет цены в выбранной валюте

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -42,6 +42,7 @@
 <!--about-end-->
 <!--product-starts-->
 <?php if ($hits): ?>
+<?php $curr = \simFW\App::$app->getProperty('currency'); ?>
 <div class="product">
     <div class="container">
         <div class="product-top">
@@ -53,9 +54,9 @@
                             <div class="product-bottom">
                                 <h3><a href="<product/<?=$hit->alias; ?>"><?=$hit->title; ?></a></h3>
                                 <p>Explore Now</p>
-                                <h4><a class="add-to-cart-link" href="cart/add?id=<?= $hit->id; ?>"><i></i></a> <span class=" item_price">$ <?= $hit->price; ?></span>
+                                <h4><a class="add-to-cart-link" href="cart/add?id=<?= $hit->id; ?>"><i></i></a> <span class=" item_price"><?= $curr['symbol_left']; ?> <?= $hit->price * $curr['value'];; ?> <?= $curr['symbol_right']; ?></span>
                                     <?php if ($hit->old_price): ?>
-                                        <small><del><?= $hit->old_price; ?></del></small>
+                                        <small><del><?= $curr['symbol_left']; ?><?= $hit->old_price * $curr['value']; ?> <?= $curr['symbol_left']; ?></del></small>
                                     <?php endif; ?>
                                 </h4>
                             </div>
