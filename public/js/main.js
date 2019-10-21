9beb081129b22c6f5d5d9dfa23b96badaa0552b1
inCrafts <inCrafts@mail.ru>
2019-10-21 15:13:21 +0300

    Добавлен контроллер корзины

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,38 @@
+// Cart scripts
+
+$('body').on('click', '.add-to-cart-link', function (e) {
+    e.preventDefault();
+    var id = $(this).data('id'),
+        qty = $('.quantity input').val() ? $('.quantity input').val() : 1,
+        mod = $('.available select').val();
+    $.ajax({
+        url: '/cart/add',
+        data: {id: id, qty: qty, mod: mod},
+        type: 'GET',
+        success: function(res) {
+            showCart(res);
+    },
+        error: function () {
+            alert('Ошибка! Попробуйте позже');
+        }
+    });
+});
+
+function showCart(cart) {
+    console.log(cart);
+}
+// Cart scripts
+
+
+// Currency scripts
+
 $('#currency').change(function () {
     window.location = 'currency/change?curr=' + $(this).val();
     console.log($(this).val());
 });
+// Currency scripts
+
+// Modifications scripts
 
 $('.available select').on('change', function () {
    let modId = $(this).val(),
@@ -14,3 +45,4 @@ $('.available select').on('change', function () {
         $('#base-price').text(symbolLeft + basePrice + symbolRight);
     }
 });
+// Modifications scripts
